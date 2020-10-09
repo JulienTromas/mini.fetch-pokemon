a59e2a362bdfefdace9181f879b3011488a74215
Julien Tromas <julien.tromas@gmail.com>
2020-10-09 15:12:33 +0900

    Wrote the findNames method and it passes all the tests
    Co-authored-by: happymanju <happymanju@users.noreply.github.com>
    Co-authored-by: JulienTromas <julientromas@users.noreply.github.com>

--- pokemonager.js
+++ pokemonager.js
@@ -2,7 +2,22 @@
   class Pokemonager {
     // This should return an array of all the names of n Pokemon from the Pokemon API.
     findNames(n) {
-      // Your code here.
+
+      let data = fetch(`https://pokeapi.co/api/v2/pokemon?limit=${n}`)
+        .then((response)=>{
+          let newResponse = response.json();
+          return newResponse;
+        })
+        .then((promiseJson) => {
+          return promiseJson.results;
+        })
+        .then( (gottaArrayThemAll) => {
+          let arrayOfPokemon = [];
+          gottaArrayThemAll.map((pokemon) => {
+            return arrayOfPokemon.push(pokemon.name) } );
+          return arrayOfPokemon;
+          });
+          return data;
     }
 
     // This should return an array of all the Pokemon that are under a particular weight.
@@ -11,6 +26,8 @@
       // Your code here.
       // ** LIMIT TO THE FIRST 10 POKEMON
       // We don't want to make too many unnecessary calls to the Pokemon API
+
+
     }
   }
 
